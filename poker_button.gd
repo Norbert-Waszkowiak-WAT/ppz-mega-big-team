5fd733f3e2761b2350816c464e8fb95ad50ebc7e
Stellusia007 <natalia.blaszczykowska@gmail.com>
2025-02-19 12:48:13 +0100

    juz wszytsko dzizała

--- poker_button.gd
+++ poker_button.gd
@@ -1,21 +1,17 @@
 extends Button
 
-var global
-
-var label_node2
-
+@onready var label_brak_kasy = $Label2
+@onready var global = get_node("/root/Global")  # Pobranie singletona Global
 
 func _ready():
-	# Znajdź node tekstu (Label)
-	label_node2 = get_node("/root/Casino/Button/cost")
-	connect("pressed", _on_pressed)
-	global = get_node("/root/Global")  # Jeśli używasz singletona do przechowywania zmiennej `coin`
-	
+	label_brak_kasy.hide()  # Ukryj label na start
+	connect("pressed", _on_pressed)  # Poprawna składnia połączenia sygnału
+
 func _on_pressed():
 	if global.coin < 10:
-		label_node2.text = "Nie masz wystarczająco pieniędzy"
-		await get_tree().create_timer(3).timeout
-		label_node2.text = "*Cost: 10 coins*"
+		label_brak_kasy.show()
+		await get_tree().create_timer(1.76).timeout
+		label_brak_kasy.hide()
 	else:
 		global.coin -= 10
-		get_tree().change_scene_to_file("res://scenes/poker_game.tscn")  # Upewnij się, że ścieżka jest poprawna
+		get_tree().change_scene_to_file("res://scenes/poker_game.tscn")  # Sprawdź poprawność ścieżki
